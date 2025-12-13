012db0ce856fd44f395f70afee14ca2c09a3823c
Mohamed Nabil <mnbil0680@gmail.com>
2025-12-13 21:10:16 +0200

    feat(app): integrate User and Tasks components, add user selection functionality

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,23 @@
 import { Component, signal } from '@angular/core';
 import { Header } from "./components/header/header";
+import { DUMMY_USERS } from "./components/user/dummy-users";
+import { User } from './components/user/user';
+import { Tasks } from "./components/tasks/tasks";
 
 @Component({
   selector: 'app-root',
-  imports: [Header],
+  imports: [Header, User, Tasks],
   templateUrl: './app.html',
 })
 export class App {
+ nameToDisplay:string = "";
+onSelectUser(name:string) {
+  console.log("Selected User With Id ==> "+ name);
+  this.nameToDisplay = name;
+
+
+}
+
   protected readonly title = signal('tmp');
+ user= DUMMY_USERS;
 }
